431c916618272419efabf73c5881085b11f94fd7
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-10-14 12:36:07 +0530

    cors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,9 +3,11 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleWare = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(" ")[1];
-        if (!token)
+        let token = req.headers.authorization;
+        if (!token) {
             return res.status(401).json("No authentication token, authorization denied.");
+        }
+        token = req.headers.authorization.split(" ")[1];
 
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         if (!verified)
